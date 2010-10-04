209d23ee4b993ee73e19db76577a804522109cbf
Lee Olayvar <leeolayvardvcs@gmail.com>
2010-10-03 18:17:49 -0700

    Added the basic functioning code. Note that it is hacky, and needs to be improved at a later date.

--- freshentipfy/scripts.py
+++ freshentipfy/scripts.py
@@ -1,7 +1,27 @@
 ''''''
 
+import sys
+import os
+
 import nose
+from nosegae import NoseGAE
+from freshen.noseplugin import FreshenNosePlugin
 
 
 def freshen_tipfy():
-    pass
+    # Until nosegae is patched, it seems we need to chdir into app
+    # for some things to work properly. (Template lookups, etc.)
+    os.chdir('app')
+    
+    # This is a hacky way to set the default arguments. It's only temporary.
+    arguments = sys.argv
+    arguments.extend([
+        '--with-freshen',
+        
+        '--with-gae',
+        '--gae-lib-root=../etc/parts/google_appengine',
+        
+        'features',])
+    
+    # Run nose.
+    nose.run(argv=arguments)
